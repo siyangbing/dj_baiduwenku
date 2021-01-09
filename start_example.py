ce8a5dc0e99a316f1bc61b3d3d1cadcf5b80d8f8
siyangbing <2240984380@qq.com>
2021-01-09 17:23:06 +0800

    v1

--- start_example.py
+++ start_example.py
@@ -1,8 +1,5 @@
 
 import os
 
-async def app(scope, receive, send):
-    ...
-
 if __name__ == "__main__":
     os.system("gunicorn -c example_conf.py dj_baiduwenku.asgi:application -k uvicorn.workers.UvicornWorker")
